856def3b6fb43c5cbad146e512886d3b751f5728
BVBFD <lsevina126@gmail.com>
2022-08-04 16:03:57 +0900

    svelt bind event and createEventDispatcher

---
+++ src/lib/FilterUser.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  const dispatch = createEventDispatcher();
+  const filter = (e) => {
+    dispatch('filter', e.target.value);
+  };
+</script>
+
+<div class="ml-4 mt-4">
+  <label for="user-filter">Filter User</label>
+  <select
+    on:change={filter}
+    name="user-filter"
+    id="user-filter"
+    class="border rounded-lg px-4 py-2 ml-4"
+  >
+    <option value={null}>All</option>
+    <option value={true}>active</option>
+    <option value={false}>inactive</option>
+  </select>
+</div>
+
+<style></style>
--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,7 +1,6 @@
 <script>
-  //   import { onMount } from 'svelte';
-  //   import { fade } from 'svelte/transition';
-  //   import { count } from '../store';
+  import User from './User.svelte';
+  import FilterUser from './FilterUser.svelte';
 
   let users = [
     {
@@ -9,83 +8,49 @@
       image: 'https://cdn-icons-png.flaticon.com/128/4786/4786297.png',
       name: 'lsevina126',
       email: 'lsevina126@gmail.com',
+      active: false,
     },
     {
       id: 2,
       image: 'https://cdn-icons-png.flaticon.com/128/4786/4786282.png',
       name: 'Hacker',
       email: 'Hcker@gmail.com',
+      active: true,
     },
     {
       id: 3,
       image: 'https://cdn-icons-png.flaticon.com/128/4786/4786318.png',
       name: 'Banker',
       email: 'Banker@gmail.com',
+      active: false,
     },
   ];
 
-  // basic svelte practice
-  //   let ref;
-  //   let refHi;
-  //   let name = 'Input Value';
-  //   //   let clicks = 0;
-  //   const increaseClicks = () => {
-  //     $count += 1;
-  //   };
+  let filteredUsers = users;
 
-  //   onMount(() => {
-  //     console.log(ref);
-  //     console.log(refHi);
-  //   });
-  // basic svelte practice
-
-  const getUsers = () => {
-    return users;
+  const filter = (e) => {
+    if (e.detail === 'null') {
+      filteredUsers = users;
+      return;
+    }
+    const status = e.detail === 'true';
+    filteredUsers = users.filter((user) => {
+      return user.active === status;
+    });
   };
 </script>
 
 <div>
-  <h1>List of Users</h1>
-  {#each getUsers() as { image, name, email, id }, i (id)}
-    <div>
-      <p>{i + 1}</p>
-      <img src={image} alt={name} />
-      <p>{name}</p>
-      <p>{email}</p>
-    </div>
+  <h1 class="text-2xl text-center mt-10">List of Users</h1>
+
+  <FilterUser on:filter={filter} />
+
+  {#each filteredUsers as user, i (user.id)}
+    <User {user} {i} />
   {:else}
     <p>No User Found!</p>
   {/each}
-
-  <!-- basic svelte practice -->
-  <!-- <div bind:this={ref}>
-    <span bind:this={refHi}>Hi</span>
-    <input type="text" bind:value={name} />
-
-    {#if name === ''}
-      <h2 transition:fade>What is your name?</h2>
-    {:else}
-      <h2>Hello {name} !</h2>
-    {/if}
-
-    <h2>Total Clicks: {$count}</h2>
-    <button on:click|once={increaseClicks}>Click me!</button>
-  </div> -->
-  <!-- basic svelte practice -->
 </div>
 
 <style>
-  /* basic svelte practice */
-  /* button {
-    border: 0;
-    background: tomato;
-    color: white;
-    padding: 5px 20px;
-    cursor: pointer;
-  }
-
-  button:active {
-    opacity: 0.5;
-  } */
-  /* basic svelte practice */
 </style>
